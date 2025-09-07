4213d35b390bb28ae6ca302043a9b1373125f59d
Tyuiu.AbramushkinAN@outlook.com <Tyuiu.AbramushkinAN@outlook.com>
2025-09-07 14:43:14 +0500

    lecture 2 complete

---
+++ TodoServerApp/Data/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoServerApp.Data
+{
+    public class TaskItem
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? FinishDate { get; set; }
+    }
+}
